8a2fb1b41efd00db49445eff3c5af8141b7ce34e
Harkály Gergő <git@harkalygergo.hu>
2025-10-06 13:01:10 +0200

    routes for main functions

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\FriendController;
+use App\Http\Controllers\MessageController;
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/users', [FriendController::class, 'index']);
+    Route::post('/friend/{user}', [FriendController::class, 'add']);
+
+    Route::post('/message/{recipient}', [MessageController::class, 'send']);
+    Route::get('/messages/{user}', [MessageController::class, 'conversation']);
+});
